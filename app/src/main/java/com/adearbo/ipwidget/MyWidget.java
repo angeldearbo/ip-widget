3212446d8f69bc7ab73854efbc1c4cc9f87e0b2a
angel <angel@brookesiadt.com>
2016-05-05 14:16:43 +0200

    Refresh fixed, gradle version updated

--- app/src/main/java/com/adearbo/ipwidget/MyWidget.java
+++ app/src/main/java/com/adearbo/ipwidget/MyWidget.java
@@ -2,9 +2,11 @@ package com.adearbo.ipwidget;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.wifi.SupplicantState;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
@@ -17,23 +19,13 @@ public class MyWidget extends AppWidgetProvider {
     private static AppWidgetManager mAppWidgetManager = null;
     private static Context mContext = null;
 
-
-    public void update() {
-        if (mAppWidgetManager != null) {
-            RemoteViews remoteViews = new RemoteViews(mContext.getPackageName(), R.layout.widget);
-            ComponentName componentName = new ComponentName(mContext, MyWidget.class);
-            remoteViews.setTextViewText(R.id.ipwifi_iptext, getWifiIp());
-
-            mAppWidgetManager.updateAppWidget(componentName, remoteViews);
-        }
-    }
-
     public void onDeleted(Context var1, int[] var2) {
         super.onDeleted(var1, var2);
     }
 
     public void onDisabled(Context var1) {
         super.onDisabled(var1);
+        mContext.getApplicationContext().unregisterReceiver(receiver);
     }
 
     public void onEnabled(Context var1) {
@@ -47,10 +39,20 @@ public class MyWidget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         mContext = context;
         mAppWidgetManager = appWidgetManager;
+        mContext.getApplicationContext().registerReceiver(receiver, new IntentFilter("android.net.wifi.STATE_CHANGE"));
         super.onUpdate(context,appWidgetManager,appWidgetIds);
         update();
     }
 
+    public void update() {
+        if (mAppWidgetManager != null) {
+            RemoteViews remoteViews = new RemoteViews(mContext.getPackageName(), R.layout.widget);
+            ComponentName componentName = new ComponentName(mContext, MyWidget.class);
+            remoteViews.setTextViewText(R.id.ipwifi_iptext, getWifiIp());
+
+            mAppWidgetManager.updateAppWidget(componentName, remoteViews);
+        }
+    }
 
     private  String getWifiIp() {
         WifiManager mWifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
@@ -70,4 +72,13 @@ public class MyWidget extends AppWidgetProvider {
         return (( i & 0xFF)+ "."+((i >> 8 ) & 0xFF)+
                 "."+((i >> 16 ) & 0xFF)+"."+((i >> 24 ) & 0xFF));
     }
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.e("ip-widget", "receiver");
+            update();
+        }
+    };
+
 }
---
+++ app/src/main/java/com/adearbo/ipwidget/NetworkChangeReceiver.java
@@ -0,0 +1,8 @@
+package com.adearbo.ipwidget;
+
+/**
+ * Created by angel on 5/04/16.
+ */
+public class NetworkChangeReceiver {
+
+}
